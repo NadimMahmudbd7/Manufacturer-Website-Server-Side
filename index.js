b4c4811432f4690e194c00956aed6d2d98a94244
Nadim Mahmud <giftsframebd@gmail.com>
2022-05-26 01:47:06 +0600

    delivery and add complete

--- index.js
+++ index.js
@@ -25,22 +25,22 @@ console.log("db connected");
 
 
 
-function VerifyJwt (req, res, next) {
+function VerifyJwt(req, res, next) {
     const getToken = req.headers.authorization
-    
+
     if (!getToken) {
         return res.status(401).send({ Message: "unauthorized access" })
     }
     else {
         const token = getToken.split(" ")[1]
         jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-           if(err){
-               console.log(err)
-           }
-        
-                req.decoded = decoded
-                next()
-           
+            if (err) {
+                console.log(err)
+            }
+
+            req.decoded = decoded
+            next()
+
         });
     }
 }
@@ -49,38 +49,38 @@ async function run() {
     try {
         await client.connect()
 
-        app.get("/products", async(req,res)=>{
+        app.get("/products", async (req, res) => {
             const products = await TotalServiceCollections.find({}).toArray()
-            res.send ({success:"successfully uploaded products", products})
+            res.send({ success: "successfully uploaded products", products })
         })
 
-        app.get("/reviews",  async(req,res)=>{
+        app.get("/reviews", async (req, res) => {
             const reviews = await TotalReviewsCollections.find({}).toArray()
-            res.send ({success:"successfully uploaded reviews", reviews})
+            res.send({ success: "successfully uploaded reviews", reviews })
         })
 
-        app.get("/payment/:id", async(req,res)=>{
+        app.get("/payment/:id", async (req, res) => {
             const id = req.params.id
-           const query = {_id:ObjectId(id)}
-           const result = await TotalServiceCollections.findOne(query)
-           res.send({success:"get successfully",result})
+            const query = { _id: ObjectId(id) }
+            const result = await TotalServiceCollections.findOne(query)
+            res.send({ success: "get successfully", result })
         })
 
-        app.get("/users", VerifyJwt,  async(req,res)=>{
+        app.get("/users", VerifyJwt, async (req, res) => {
             const email = req?.query?.email
             const user = req.query.user
             const decodedEmail = req.decoded;
-           
-            if(email === decodedEmail){
+
+            if (email === decodedEmail) {
                 const result = await TotalUsers.find().toArray()
-               return res.send({success:"success", result})
+                return res.send({ success: "success", result })
             }
-            else{
-                return res.send({success:"error"})
+            else {
+                return res.send({ success: "error" })
             }
         })
 
-        app.put("/users/:email", async(req,res)=>{
+        app.put("/users/:email", async (req, res) => {
             const email = req.params.email
             const filter = { email: email }
             const user = req.body
@@ -93,71 +93,72 @@ async function run() {
         })
 
 
-        app.get("/user", async(req,res)=>{
+        app.get("/user", async (req, res) => {
             const email = req.query.email
-            const filter = {email:email}
+            const filter = { email: email }
             const user = await TotalUsers.findOne(filter)
             res.send(user)
         })
 
-        app.get('/myorders', VerifyJwt, async(req, res) => {
+        app.get('/myorders', VerifyJwt, async (req, res) => {
             const email = req?.query?.email
-           
+
             const decodedEmail = req.decoded;
-            
-           
-            if(email === decodedEmail){
-                const filter ={email:email}
+
+
+            if (email === decodedEmail) {
+                const filter = { email: email }
                 const result = await TotalOrder.find(filter).toArray()
-               return res.send({success:"success", result})
+                return res.send({ success: "success", result })
             }
-            else{
-                return res.send({success:"error"})
+            else {
+                return res.send({ success: "error" })
             }
 
         })
 
 
-        app.post("/addproducts", async(req,res)=>{
+
+        app.post("/addproducts", async (req, res) => {
             const filter = req.body
             const products = await TotalServiceCollections.insertOne(filter)
-            res.send({success:"products uploaded", products})
+            res.send({ success: "products uploaded", products })
         })
 
-        app.post("/reviews", async(req,res)=>{
+        app.post("/reviews", async (req, res) => {
             const filter = req.body
             const reviews = await TotalReviewsCollections.insertOne(filter)
-            res.send({success:"review uploaded", reviews})
+            res.send({ success: "review uploaded", reviews })
         })
 
-        app.post("/orders", async(req,res)=>{
+        app.post("/orders", async (req, res) => {
             const filter = req.body
             const order = await TotalOrder.insertOne(filter)
-            res.send({success:"review uploaded", order})
+            res.send({ success: "review uploaded", order })
         })
-        app.put("/orders", async(req,res)=>{
+        app.put("/orders", async (req, res) => {
             const id = req.query.id
-            const filter = {_id:ObjectId(id)}
+            const filter = { _id: ObjectId(id) }
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    payment:"paid"
+                    payment: "paid"
                 }
             };
             const payment = await TotalOrder.updateOne(filter, updateDoc, options);
-            res.send({success:"payment done", payment})
+            res.send({ success: "payment done", payment })
         })
-        app.put("/order", async(req,res)=>{
+        app.put("/order", async (req, res) => {
             const id = req.query.id
-            const filter = {_id:ObjectId(id)}
+            const filter = { _id: ObjectId(id) }
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    shipping:"confirm"
+                    shipping: "confirm"
                 }
             };
             const payment = await TotalOrder.updateOne(filter, updateDoc, options);
-            res.send({success:"payment done", payment})
+            res.send({ success: "payment done", payment })
         })
 
 
@@ -176,18 +177,18 @@ async function run() {
         app.put("/user/admin/:email", VerifyJwt, async (req, res) => {
             const email = req.params.email
             const requester = req.decoded
-            const requesterAccount = await TotalUsers.findOne({email:requester})
-            if(requesterAccount.Role === "admin"){
+            const requesterAccount = await TotalUsers.findOne({ email: requester })
+            if (requesterAccount.Role === "admin") {
 
                 const filter = { email: email }
                 const updateDoc = {
-                    $set: {Role:"admin"}
+                    $set: { Role: "admin" }
                 };
                 const result = await TotalUsers.updateOne(filter, updateDoc);
                 res.send(result)
             }
-            else{
-                res.status(403).send({Message:"Forbidden"})
+            else {
+                res.status(403).send({ Message: "Forbidden" })
             }
         })
         app.patch("/user/admin/:email", async (req, res) => {
@@ -195,24 +196,24 @@ async function run() {
             const filter = { email: email }
 
             const updateDoc = {
-                $set: {Role:""}
+                $set: { Role: "" }
             };
             const result = await TotalUsers.updateOne(filter, updateDoc);
             res.send(result)
         })
 
-        app.put("/products", async(req,res)=>{
+        app.put("/products", async (req, res) => {
             const id = req.params.id
             console.log(id);
             const filter = { _id: ObjectId(id) }
-            
+
         })
 
-        app.get("/totalorder", VerifyJwt, async(req,res)=>{
+        app.get("/totalorder", VerifyJwt, async (req, res) => {
             const orders = await TotalOrder.find().toArray()
-            res.send({success:"order collected",orders})
+            res.send({ success: "order collected", orders })
         })
-        app.put("/deliverproduct/:id",async(req,res)=>{
+        app.put("/deliverproduct/:id", async (req, res) => {
             const id = req.params.id
             let previusQty = parseInt(req.body.qty)
             if (previusQty < 1) {
@@ -220,10 +221,10 @@ async function run() {
             }
             else {
                 const deliverQty = parseInt(req.body.deliverQty)
-                if (previusQty < deliverQty || deliverQty <= 0 ) {
+                if (previusQty < deliverQty || deliverQty <= 0) {
                     res.send({ error: "Oops !! Pleasee Check Stock" })
                 }
-                
+
                 else {
                     previusQty = previusQty - deliverQty
                     const filter = { _id: ObjectId(id) }
@@ -240,12 +241,12 @@ async function run() {
         })
 
 
-        app.get("/dashboard/payment/:id", async(req,res)=>{
+        app.get("/dashboard/payment/:id", async (req, res) => {
             const id = req.params.id
-           const query = {_id:ObjectId(id)}
-           const result = await TotalOrder.findOne(query)
-           console.log(result);
-           res.send({success:"get successfully",result:result})
+            const query = { _id: ObjectId(id) }
+            const result = await TotalOrder.findOne(query)
+            console.log(result);
+            res.send({ success: "get successfully", result: result })
         })
 
         app.delete('/products/:id', async (req, res) => {
